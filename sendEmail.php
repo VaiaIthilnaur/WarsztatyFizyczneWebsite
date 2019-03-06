ee32a4e43776e2dda3055e763682032a2c0b643c
VaiaIthilnaur <35402633+VaiaIthilnaur@users.noreply.github.com>
2019-03-06 17:17:14 +0100

    Add files via upload

---
+++ sendEmail.php
@@ -0,0 +1,25 @@
+﻿<?php
+	$name=$_POST['name'];
+	$visitor_email=$_POST['email'];
+	$subject=$_POST['subject'];
+	$message=$_POST['message'];
+	
+	$email_form='email';
+	
+	$email_body="User name: $name.\n".
+						"User subject: $subject.\n";
+							
+	$to ="dobnosz@gmail.com";
+						
+						
+	$headers="From: $visitor_email \r\n"; 
+	$headers.="Replay-to: $email_form\r\n"; 
+	$headers.="Subject: $subject\r\n"; 
+	$headers.="Message: $message\r\n";
+	
+	mail($to,$email_body,$headers);
+	
+	header("Location: index.html");
+	
+	
+?>
